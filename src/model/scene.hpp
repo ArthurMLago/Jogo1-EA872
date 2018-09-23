acf986f6a3efa2b274aa7fa02dcb4fa2738681b9
Arthur Moraes do Lago <arthurmoraeslago@gmail.com>
2018-09-23 20:07:42 -0300

    Adicionado algumas propriedades da classe scene

---
+++ src/model/scene.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <vector>
+
+#include "../model/player.hpp"
+#include "../model/enemy.hpp"
+
+class Scene {
+	public:
+		std::vector<Enemy*> enemyList;
+		Player *player;
+
+		
+
+};

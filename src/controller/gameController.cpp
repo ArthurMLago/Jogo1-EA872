d2899a4a903de49f3508a9269414e4fbcc86d932
vsdsUni <vitorsaldanhadasilveira@gmail.com>
2018-09-25 23:42:37 -0300

    Update gameController.cpp
    controller final 1

--- src/controller/gameController.cpp
+++ src/controller/gameController.cpp
@@ -1,11 +1,73 @@
 #include "gameController.hpp"
 
+/*
+*Funcao chamada a cada frame do jogo, provavelmente 60 vezes por segundos
+*Deve cuidar das funcoes basicas do jogo, como criar mais inimigos bolas, fazer os inimigos se movimentarem,
+*verificar colisoes, etc
+*/
+int larg;
+int alt;
+viewController->getScreenDimension(&larg, &alt);
 void GameController::update(){
-	// cria mais bolas
+	float val = alt*((float)(rand())/RAND_MAX);
+	// a cada 2 segundos entra no if 
+	if(tempo%120 == 0){
+		// cria mais bolas
+		currentScene->Enemy* novo_enemy = new Enemy(0.0; val,get_speed());
+		currentScene->enemyList.push_back(novo_enemy);
+	}
 
-	// move as bolas que ja existem:
+	// A cada 1 segundo entra no if
+	if(tempo%60 == 0){	
+		// move as bolas que ja existem
+		for(int i = 0; i<currentScene->enemyList.size();i++){
+			currentScene->enemyList[i]->move(1.0, 0.0);
+		}
+	}
+	// loop que verificar se ocorreu alguma colisa
+	for(int i = 0; i < currentScene->enemyList.size();i++){
+		// Se ouver colisao entra no if
+		if((currentScene->enemyList[i]->get_pos_x() <= currentScene->player->get_pos_x()+0.5
+			&&currentScene->enemyList[i]->get_pos_x() >= currentScene->player->get_pos_x()-0.5) 
+				&&(currentScene->enemyList[i]->get_pos_y() <= currentScene->player->get_pos_y()+0.5
+					&&currentScene->enemyList[i]->get_pos_y() >= currentScene->player->get_pos_y()-0.5)){
+				// Flag da colisao muda
+				colisao= 1;
+				// condicao para parar o loop
+				i = currentScene->enemyList.size();
+		}
+	}
+	tempo++;
+}
 
-	// verifica colisoes
-	
+/*
+*Essa funcão atualiza a propriedade viewController para o ponteiro passado.
+*/
+void GameController::setViewController(ViewController *pointer){
+	viewController = pointer;
+}
+
+/*
+*Essa funcão atualiza a propriedade Setscene para o ponteiro passado.
+*/
+void GameController::setScene(Scene *pointer){
+	currentScene = pointer;
+}
+
+/*
+*Funcao chamada pelo viewController para avisar o gameController que o usuario apertou uma tecla para
+*ir para cima e a posição do jogador deve ser atualizada no model
+*/
+void GameController::userPressedUp(){
+	currentScene->player->move(0.0, 1.0);
 
 }
+
+/* 
+*Funcao chamada pelo viewController para avisar o gameController que o usuario apertou uma tecla para
+*ir para baixo e a posição do jogador deve ser atualizada no model
+*/
+void GameController::userPressedDown(){
+	currentScene->player->move(0.0, -1.0);
+}
+

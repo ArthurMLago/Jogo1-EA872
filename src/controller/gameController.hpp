3a11871676548f9d7c16866f9da0bfc68e84d559
vsdsUni <vitorsaldanhadasilveira@gmail.com>
2018-09-25 23:51:34 -0300

    Update gameController.hpp
    variavel tempo e colisao

--- src/controller/gameController.hpp
+++ src/controller/gameController.hpp
@@ -2,12 +2,13 @@
 
 #include "../model/scene.hpp"
 #include "../view/viewController.hpp"
-int tempo = 0; //variável global que conta o tempo começa em 0
-int colisao = 0; // variavel global que indica se ouve colisao
+
 class GameController {
 	private:
 		Scene *currentScene;
 		ViewController *viewController;
+		int tempo = 0; //variável global que conta o tempo começa em 0
+		int colisao = 0; // variavel global que indica se ouve colisao
 
 	public:
 		void setViewController(ViewController *pointer);

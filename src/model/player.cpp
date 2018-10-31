b3bde087c55567b3329a0c3929ef03a445c8de3e
vsdsUni <vitorsaldanhadasilveira@gmail.com>
2018-10-31 14:40:43 -0300

    Update player.cpp
    mudança de float para int

--- src/model/player.cpp
+++ src/model/player.cpp
@@ -1,21 +1,16 @@
-#include "player.hpp"
+#pragma once
 
-Player::Player(float pos_x,int pos_y){
-	this->pos_x = pos_x;
-	this->pos_y = pos_y;
+class Player {
+        private:
+                float pos_x;
+                int  pos_y;
 
-}
+        public:
+                Player(float pos_x, int pos_y);
 
-float Player::get_pos_x(){
-	return this->pos_x;
-}
+                float get_pos_x();
+                int get_pos_y();
+                void move(float delta_x,int delta_y);
 
-float Player::get_pos_y(){
-	return this->pos_y;
-}
-
-void Player::move(float delta_x, float delta_y){
-	this->pos_x += delta_x;
-	this->pos_y += delta_y;
-}
-
+};
+    
